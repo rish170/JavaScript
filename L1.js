b60c358b2cf3fb24f1c8f6c83cd383c02b7a55ff
Rishab Thutheja <rishab.17tt@gmail.com>
2025-01-25 14:19:49 +0530

    Lecture 1

---
+++ L1.js
@@ -0,0 +1,16 @@
+console.log("Apna College")
+
+age = 24
+name = "Rishab Thutheja"
+CGPA = 9.00
+x = null
+y = undefined
+bool = true
+
+console.log(name, age, CGPA)
+console.log(name)
+console.log(age)
+console.log(CGPA)
+console.log(x)
+console.log(y)
+console.log(bool)
