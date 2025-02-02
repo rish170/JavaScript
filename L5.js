3ffd34cb3acbc8ec9f289c8f57144b56ad0ccb5b
Rishab Thutheja <rishab.17tt@gmail.com>
2025-02-02 11:52:16 +0530

    Lecture 5

--- L5.js
+++ L5.js
@@ -32,3 +32,29 @@ console.log(marks);
 
 console.log(marks.shift());   //  Removes + returns first el of array
 console.log(marks);
+
+
+arr = [0,1,2,3,4,5,6,7,8];
+console.log(arr.slice(1,4));  // Returns Slice. Last index not included.
+
+// splice(startIndex , deleteCount , newElement1, newElement2, newElement3, ...)
+/* 
+   1. startIndex = Index to start making changes from
+   2. deleteCount = No. of elements to delete starting from startIndex
+   3. newElement(i) = New element to add from startIndex after deleting
+*/
+
+arr.splice(2,2)  // [0,1,4,5,6,7,8]  From index 2 delete two elements. Index 2 and 3 (El: 2,3)
+console.log(arr);
+
+arr.splice(2,0,101,102)  // [0,1,101,102,4,5,6,7,8]  At 2, delete 0 (Nothing), add 101 and 102
+console.log(arr);
+
+arr.splice(3,1) // [0,1,101,4,5,6,7,8]  Delete element at 3rd index
+console.log(arr);
+
+arr.splice(3,1,101) // [0,1,101,101,5,6,7,8]  Replace element at 3rd index
+console.log(arr);
+
+console.log(arr.splice(4)) // Returns [5,6,7,8] and deletes this part from arr. Final arr = [0,1,101,101]
+console.log(arr);
